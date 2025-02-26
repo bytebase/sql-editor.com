791b2d2624510dee4dffb42eb736731b30b1ab58
tianzhou <t@bytebase.com>
2025-02-26 19:01:15 +0800

    chore: add plausible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,25 +7,28 @@ import { PostHogPageview } from "@/components/PosthogProvider";
 import "@/styles/global.css";
 import { getMetadata } from "@/utils/metadata";
 import { fontLoader } from "./fonts";
+import PlausibleProvider from 'next-plausible'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={fontLoader()}>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
-        <link rel="icon" href="/logo.png" type="image/png" />
-      </head>
-      <body>
-        <div className="relative w-full min-h-[100svh] flex flex-col bg-white">
-          <Header />
-          <main className="shrink-0 grow basis-auto mx-auto pt-6 pb-16 w-full flex flex-col justify-start items-center">{children}</main>
-          <Footer />
-        </div>
-        <SpeedInsights />
-        <Suspense>
-          <PostHogPageview />
-        </Suspense>
-      </body>
+      <PlausibleProvider domain="sql-editor.com">
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
+          <link rel="icon" href="/logo.png" type="image/png" />
+        </head>
+        <body>
+          <div className="relative w-full min-h-[100svh] flex flex-col bg-white">
+            <Header />
+            <main className="shrink-0 grow basis-auto mx-auto pt-6 pb-16 w-full flex flex-col justify-start items-center">{children}</main>
+            <Footer />
+          </div>
+          <SpeedInsights />
+          <Suspense>
+            <PostHogPageview />
+          </Suspense>
+        </body>
+      </PlausibleProvider>
     </html>
   );
 };
